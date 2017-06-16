4e4bc6a2d3c3b42f0fe1b7c35800e0797fcdb586
Rudi Lee <quadhack@gmail.com>
2017-06-16 15:14:53 +0700

    feat: Show loading progressbar while retrieve Movie list

--- app/src/main/java/com/example/android/popularmovies/MainActivity.java
+++ app/src/main/java/com/example/android/popularmovies/MainActivity.java
@@ -5,6 +5,9 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.view.animation.AlphaAnimation;
+import android.widget.FrameLayout;
 
 import java.io.IOException;
 import java.util.List;
@@ -16,6 +19,7 @@ import retrofit2.converter.moshi.MoshiConverterFactory;
 public class MainActivity extends AppCompatActivity {
     private List<MovieDetail> mMovieDetails = null;
     private MovieThumbnailAdapter mMovieThumbnailAdapter = new MovieThumbnailAdapter();
+    private FrameLayout mLoadingHolder;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +31,8 @@ public class MainActivity extends AppCompatActivity {
         movieThumbnailsRecyclerView.setLayoutManager(new GridLayoutManager(this, 2));
         movieThumbnailsRecyclerView.setAdapter(mMovieThumbnailAdapter);
 
+        mLoadingHolder = (FrameLayout) findViewById(R.id.loading_holder);
+
         loadMovieList();
     }
 
@@ -34,8 +40,26 @@ public class MainActivity extends AppCompatActivity {
         new LoadMovieListTask().execute();
     }
 
+    private void toggleLoading(boolean loading) {
+        float fromAlpha = loading ? 0f : 1f;
+        float toAlpha = loading ? 1f : 0f;
+
+        AlphaAnimation animation = new AlphaAnimation(fromAlpha, toAlpha);
+        animation.setDuration(200);
+
+        mLoadingHolder.setAnimation(animation);
+        mLoadingHolder.setVisibility(loading ? View.VISIBLE : View.GONE);
+    }
+
     private class LoadMovieListTask extends AsyncTask<String, Void, List<MovieDetail> > {
         final static String TMDB_BASE_URL = "https://api.themoviedb.org/3/";
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+
+            toggleLoading(true);
+        }
 
         @Override
         protected List<MovieDetail> doInBackground(String... params) {
@@ -62,10 +86,14 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         protected void onPostExecute(List<MovieDetail> movieDetails) {
+            super.onPostExecute(movieDetails);
+
             if (movieDetails != null) {
                 mMovieDetails = movieDetails;
                 mMovieThumbnailAdapter.setMovieDetails(movieDetails);
             }
+
+            toggleLoading(false);
         }
     }
 }
