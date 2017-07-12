693b3509694762a9c8c6cb90cabac4fdd93d896c
Rudi Lee <quadhack@gmail.com>
2017-07-12 11:16:29 +0700

    refactor: Rename The Movie DB base url constant
    And adding backdrop image size constant

--- app/src/main/java/com/example/android/popularmovies/TheMovieDb.java
+++ app/src/main/java/com/example/android/popularmovies/TheMovieDb.java
@@ -5,8 +5,9 @@ package com.example.android.popularmovies;
  */
 
 class TheMovieDb {
-    public static final String TMDB_POSTER_BASE_URL = "https://image.tmdb.org/t/p/";
+    public static final String TMDB_IMAGE_BASE_URL = "https://image.tmdb.org/t/p/";
     public static final String TMDB_POSTER_SIZE = "w342";
+    public static final String TMDB_BACKDROP_SIZE = "w780";
     public final static String TMDB_BASE_URL = "https://api.themoviedb.org/3/";
     public static final String POPULARITY_ASC = "popularity.asc";
     public static final String POPULARITY_DESC = "popularity.desc";
